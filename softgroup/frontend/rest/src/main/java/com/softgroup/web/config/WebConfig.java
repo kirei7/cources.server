c984eba59ef8ab071da325e7cd4bbcee57427c17
kirei7 <vlad.sereda.y@gmail.com>
2017-03-26 10:54:49 +0300

    Create rest module

---
+++ softgroup/frontend/rest/src/main/java/com/softgroup/web/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.softgroup.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.softgroup.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+}
